208120c2c66962f69f717faa6e45837c5feb778d
Gui-Luz <guilhermeluzsaldanha@gmail.com>
2022-05-14 19:45:49 -0300

    Add warehouse factory and events

--- contracts/Item.sol
+++ contracts/Item.sol
@@ -3,31 +3,45 @@ pragma solidity 0.8.0;
 
 contract Item {
     address public warehouse;
+    string public product;
     bytes32 public identification;
     bool public available;
     uint public price;
+    uint public lastUpdated;
     address public buyer;
     enum Status { InStock, ReadyForShipping, Shipped }
     Status public choice;
+    event itemUpdated(address itemAddress, string product, address buyer, uint lastUpdated, Status choice);
 
-    constructor(string memory _identification, uint _price) {
+    constructor(string memory _product, string memory _identification, uint _price) {
+        product = _product;
         available = true;
         warehouse = msg.sender;
         identification = sha256(bytes(_identification));
         price = _price;
         choice = Status.InStock;
+        lastUpdated = block.timestamp;
     }
 
     function buy() public payable {
-        require(available);
+        require(available, "Item is not available");
         require(msg.value == price, "Value sent is not equal to price");
         buyer = msg.sender;
         choice = Status.ReadyForShipping;
         available = false;
+        lastUpdated = block.timestamp;
+        emit itemUpdated(address(this), product, buyer, lastUpdated, choice);
     }
 
     function ship() public {
         require(msg.sender == warehouse, "You are not the warehouse of this product");
+        transferPayment();
         choice = Status.Shipped;
+        lastUpdated = block.timestamp;
+        emit itemUpdated(address(this), product, buyer, lastUpdated, choice);
+    }
+
+    function transferPayment() private {
+        payable(warehouse).transfer(address(this).balance);
     }
 }
--- contracts/Warehouse.sol
+++ contracts/Warehouse.sol
@@ -7,22 +7,25 @@ import "./Item.sol";
 contract Warehouse {
     address public manager;
     string public description;
+    uint public lastUpdated;
     address[] stock;
     address[] soldItems;
     mapping (address => uint) itemIndex;
     uint index;
     Item item;
+    event itemCreation(address itemAddress);
 
-    constructor(string memory _description) {
-        manager = msg.sender;
+    constructor(string memory _description, address _manager) {
+        manager = _manager;
         description = _description;
     }
 
-    function createItem(string memory _identification, uint _price) onlyManager public {
-        Item newItem = new Item(_identification, _price);
+    function createItem(string memory _product, string memory _identification, uint _price) onlyManager public {
+        Item newItem = new Item(_product, _identification, _price);
         stock.push(address(newItem));
         itemIndex[address(newItem)] = index;
         index++;
+        emit itemCreation(address(newItem));
     }
 
     function getItemsInStock() public view returns (address[] memory) {
@@ -38,6 +41,15 @@ contract Warehouse {
         item.ship();
         soldItems.push(_existingItemAddress);
         delete stock[itemIndex[_existingItemAddress]];
+    }
+
+    function transferPayment(address _recevingAccount) onlyManager public {
+        payable(_recevingAccount).transfer(address(this).balance);
+    }
+
+
+    receive() payable external {
+        require(itemIndex[msg.sender] >= 0, "You are not alowed to pay this warehouse");
     }
 
     modifier onlyManager() {
